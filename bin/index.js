21b22107d726e4c2ef88b0182569f34785f0e56c
Neil Grogan <neil@grogan.ie>
2017-07-19 12:34:11 +0100

    cleaned up code indentation

--- bin/index.js
+++ bin/index.js
@@ -5,27 +5,26 @@ const cmd = 'pdftotext'
 const opts = ' -enc UTF-8 - '
 
 exports.handler = (event, context, callback) => {
-	console.log('incoming: ', event)
-
-	if (!event.file) {
-        return callback('Please upload a pdf file to use');
-    }
+  console.log('incoming: ', event)
+  if (!event.file) {
+      return callback('Please upload a pdf file to use');
+  }
 	
-	fs.writeFile("/tmp/test.pdf", event.pdf, function (err) {
-        if (err) {
-			return callback("writeFile failed: " + err);
-        } else {
-            //context.succeed("writeFile succeeded")
-			const child = exec(cmd + event.file + opts, (error) => {
-				// Resolve with result of process
-				callback(error, 'Process complete!');
-			})
+  fs.writeFile("/tmp/test.pdf", event.pdf, function (err) {
+    if (err) {
+      return callback("writeFile failed: " + err);
+    } else {
+      //context.succeed("writeFile succeeded")
+      const child = exec(cmd + ' ./test/li-test.pdf ' + opts, (error) => {
+        // Resolve with result of process
+        callback(error, 'Process complete!');
+      })
 
-			// Log process stdout and stderr
-			child.stdout.on('data', console.log)
-			child.stderr.on('data', console.error)
-        }
-    });
+      // Log process stdout and stderr
+      child.stdout.on('data', console.log)
+      child.stderr.on('data', console.error)
+    }
+  });
 
     
 }

c538f23550d94a105d03532f1cd2a3799e1804c7
Neil Grogan <neil@grogan.ie>
2017-07-24 11:43:53 +0100

    Added form mulipart support & upload to S3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'] + '/dist/'
 import PDFToText from './src/pdf';
 import FormHandler from './src/form-handler';
+var multipart = require("parse-multipart");
 
 const headers = {
   'Content-Type': 'application/json',
@@ -9,15 +10,34 @@ const headers = {
   'Access-Control-Allow-Headers': 'Content-Type'
 };
 
+function processPayload(event){
+  const result = {};
+  const contentType = event.headers['content-type'] || event.headers['Content-Type'];
+
+  if(contentType === 'application/pdf'){
+    result.filename = 'test.pdf'
+    result.data = new Buffer(event.body,'base64');
+    return result;
+  }
+
+  if(event.hasOwnProperty('body-json')) {
+    var bodyBuffer = new Buffer(event['body-json'].toString(),'base64');
+    var boundary = multipart.getBoundary(event.params.header['content-type']);
+    var parts = multipart.Parse(bodyBuffer,boundary)[0];
+    return parts;
+  }
+}
+
 exports.handler = async (event, context, callback) => {
   try {
 
-    const f = new FormHandler(event);
-    const form = await f.run();
-    console.log('Form Response is: ' + JSON.stringify(form));
-    const controller = new PDFToText(form.filename, form.file);
+    console.log(`Event is: ${JSON.stringify(event)}`);
+    const config = processPayload(event);
+    console.log('Form Response is: ' + JSON.stringify(config));
+
+    const controller = new PDFToText(config);
     const response = await controller.run();
-    console.log('PDF Response is: ' + response);
+    console.log(`PDF Response is: ${response}`);
 
     return context.succeed({ statusCode: 200, body: response, headers: headers });
 
--- src/pdf.js
+++ src/pdf.js
@@ -12,13 +12,39 @@ export default class PDFToText
    * @param {fileName} - name of the file
    * @param {fileData} - a buffer representing file contents
    */
-  constructor(fileName, fileData) {
-    this.fileName = fileName;
-    this.fileData = fileData;
+  constructor(config) {
+    this.fileName = config.filename;
+    this.fileData = config.data;
     this.filePath = path.join(os.tmpDir(), path.basename(this.fileName));
     console.log(`PDFToText init with: ${this.fileName} ${this.filePath} ${JSON.stringify(this.fileData)}`);
   }
 
+  async write_s3(filePath) {
+    return new Promise((resolve, reject) => {
+      const AWS = require('aws-sdk');
+
+      AWS.config.region = 'eu-west-1';
+      const s3 = new AWS.S3();
+      console.log('Going to call S3');
+      
+      var params = {
+          Bucket: 'gbspdf',
+          Key: 'test.pdf',
+          Body: fs.readFileSync(filePath),
+          ContentType: 'application/octet-stream'
+      };
+
+      s3.upload(params, function (err, res) {               
+        if(err) {
+          console.log("Error in uploading file on s3 due to "+ err)
+          reject(err);
+        } else {    
+          console.log("File successfully uploaded.")
+          resolve();
+        }
+      });
+    });
+  }
   /**
   * Write a buffer to disk
   * @param {filePath} - path to the file
@@ -72,6 +98,7 @@ export default class PDFToText
    */
   async run() {
     await this.write_file(this.filePath, this.fileData);
+    // await this.write_s3(this.filePath);
     const pdfText = await this.get_text(this.filePath);
     console.log('Returning from PDFToText: ' + pdfText);
 

da28413192a7f6c7a68edfb99d1034bbd34ab3ec
Neil Grogan <neil@grogan.ie>
2017-08-22 18:04:24 +0100

    Slim down erraneous code

--- index.js
+++ index.js
@@ -1,42 +1,31 @@
-const multipart = require("parse-multipart");
-process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'] + '/dist/'
+const multipart = require('parse-multipart');
+process.env['PATH'] =
+  process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'] + '/dist/';
 const log = require('winston');
-log.level = process.env.LOG_LEVEL
+log.level = process.env.LOG_LEVEL;
 import PDFToText from './src/pdf';
-import FormHandler from './src/form-handler';
-import Bills from './src/bills';
-import Dropbox from './src/dropbox';
-import bills_config from './config';
 
-
-const headers = {
-  'Content-Type': 'application/json',
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'OPTIONS, POST',
-  'Access-Control-Allow-Headers': 'Content-Type'
-};
-
-function processPayload(event){
+function processPayload(event) {
   const result = {};
-  const contentType = event.headers['content-type'] || event.headers['Content-Type'];
+  const contentType =
+    event.headers['content-type'] || event.headers['Content-Type'];
 
-  if(contentType === 'application/pdf'){
-    result.filename = 'test.pdf'
-    result.data = new Buffer(event.body,'base64');
+  if (contentType === 'application/pdf') {
+    result.filename = 'test.pdf';
+    result.data = new Buffer(event.body, 'base64');
     return result;
   }
 
-  if(event.hasOwnProperty('body-json')) {
-    var bodyBuffer = new Buffer(event['body-json'].toString(),'base64');
+  if (event.hasOwnProperty('body-json')) {
+    var bodyBuffer = new Buffer(event['body-json'].toString(), 'base64');
     var boundary = multipart.getBoundary(event.params.header['content-type']);
-    var parts = multipart.Parse(bodyBuffer,boundary)[0];
+    var parts = multipart.Parse(bodyBuffer, boundary)[0];
     return parts;
   }
 }
 
 exports.handler = async (event, context, callback) => {
   try {
-
     log.debug(`Event is: ${JSON.stringify(event)}`);
     const config = processPayload(event);
     log.debug('Form Response is: ' + JSON.stringify(config));
@@ -45,18 +34,17 @@ exports.handler = async (event, context, callback) => {
     const pdfResp = await controller.run();
     log.debug(`PDF Response is: ${pdfResp}`);
 
-    const bi = new Bills(pdfResp.text, bills_config['bills']);
-    const res = await bi.run();
-    log.debug(`Bill Response is: ${JSON.stringify(res)}`);
-
-    const db = new Dropbox(bills_config['DB_TOKEN']);
-    const dbResp = await db.upload(res.path, config.data)
-    log.debug(`Dropbox Response is: ${JSON.stringify(dbResp)}`);
-
-    return context.succeed({ statusCode: 200, body: pdfResp.text, headers: headers });
-
+    return context.succeed({
+      statusCode: 200,
+      body: pdfResp.text,
+      headers: {'Content-Type': 'application/json'}
+    });
   } catch (e) {
     log.error(`Application ERROR: ${e.stack}`);
-    return context.fail({ statusCode: 500, body: `Application Error: ${e}`, headers });
+    return context.fail({
+      statusCode: 500,
+      body: `Application Error: ${e}`,
+      headers
+    });
   }
 };
